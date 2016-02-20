7ea7b61626acb4155fb45445b8d8bedc1d40c933
praveensa <praveenkumar.kgm@gmail.com>
2016-02-21 02:13:51 +0530

    Templates

--- app/views.py
+++ app/views.py
@@ -1,7 +1,26 @@
+from flask import render_template
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'Hello World'
+	user= { 'nickname' : 'Praveenkumar'}
+	posts=[
+		{'author':{'nickname':'Hari'},
+		'content':'Nammooru Bengaloruuu'},
+		{'author':{'nickname':'Ara'},
+                'content':'Namma ooru madrasu ithuku naakathanae addressu'},
+		{'author':{'nickname':'Maha'},
+                'content':'Bavi ooru namma ooru'
+		}
+		]
 
+
+	return render_template('index.html',user=user,title='Home',posts=posts)
+
+
+@app.route('/login')
+def login():
+	form=LoginForm()
+	return render_template('login.html',form=form,title='sign_in')
