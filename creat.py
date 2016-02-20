f7a9a7eca5009e0478760745ab064e350a13911d
praveensa <praveenkumar.kgm@gmail.com>
2016-02-21 00:25:44 +0530

    some change

--- creat.py
+++
@@ -1,8 +0,0 @@
-from py2neo import Graph,Node , Relationship
-
-graph=Graph()
-
-user=Node("user",username="Praveen")
-post=Node("post",text="Hello world")
-
-graph.create(Relationship(user,"PUBLISHED",post))
