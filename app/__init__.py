2ab6314eacf5a7ac77021f288b3d1c5a57251ed8
praveensa <praveenkumar.kgm@gmail.com>
2016-02-26 00:07:28 +0530

    login-1

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,12 @@ app.config.from_object('config')
 db=SQLAlchemy(app)
 
 from app import views,models
+
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm=LoginManager()
+lm.init_app(app)
+oid=OpenID(app,os.path.join(basedir,'tmp')
